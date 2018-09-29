08077d8fdb07a6b8f0b214f4cbfae4f600a8afb3
Tifftastic <tifftastic.developer@gmail.com>
2018-09-29 15:37:38 -0700

    :q!

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,24 @@
 # file: helloworld.py
 
+import webbrowser
+
 my_word = raw_input('Enter a noun: ')
 
-print("HELLO WORLD, says " + my_word)
+# changing original - print("HELLO WORLD, says " + my_word)
+f = open('helloworld.html','w')
+
+greeting = """ <!Doctype HTML>
+    <html>
+    <head>
+    <title>World Greetings</title></head>
+    <body><p>Hello world, says %s</p></body>
+    </html> """
+
+greeting_world = greeting % my_word
+
+f.write(greeting_world)
+f.close()
+
+
+filename = 'file:///Users/RubbaDubDub/GitHub/hello-world' + '/helloworld.html'
+webbrowser.open_new_tab(filename)
